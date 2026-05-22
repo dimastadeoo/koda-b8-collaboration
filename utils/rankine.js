86a4de155ba206c201f742248aadcde02e97ac24
dimastadeoo <dimastadeoo@gmail.com>
2026-05-22 17:16:12 +0700

    fix: delete guarding in file celcius.js and rankine.js because guarding is already in the index.js file and fix indent in file index.js

--- utils/rankine.js
+++ utils/rankine.js
@@ -1,27 +1,15 @@
 // Implement functions here 👇
 function rankineToKelvin (value) {
-  if (typeof value === "number") {
-    return value * 5/9;
-  } else {
-    throw (new Error ("Input must be number"));
-  }
+  return value * 5/9;
 }
 
 function rankineToFahrenheit (value) { 
-  if (typeof value !== "number") { 
-    throw new Error("Input must be number"); 
-  }
   return value - 459.67;
 }
 
 function rankineToCelsius(value){
-  if(typeof value !== "number"){
-    throw new Error("Input must be number");
-  }else{
-    return (value - 491.67) * 5/9;
-  }    
+  return (value - 491.67) * 5/9;    
 }
-
 
 // TODO: Uncomment after implemented
 module.exports = {
